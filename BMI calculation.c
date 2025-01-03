413e5a2ca6451323a43f2ecf10820f122ad178f1
James28Dev <james28.dev@gmail.com>
2025-01-03 20:12:41 +0700

    BMI calculation 1.1

--- BMI calculation.c
+++ BMI calculation.c
@@ -1,48 +1,53 @@
 #include <stdio.h>
 
-int main()
-{
-	float BMI, height, weight;
-	char sex;
-
-	printf("BMI calculator\n");
-	printf("Enter sex [f/m]\t  : ");
-	scanf("%c",&sex);
-	sex = tolower(sex);
-	printf("Enter weight [kg] : ");
-	scanf("%f",&weight);
-	printf("Enter height [cm] : ");
-	scanf("%f",&height);
-	BMI = weight / ((height * 0.01) * (height * 0.01));
-
-	switch(sex)
-	{
+int calculation(char *sex, float *BMI) {
+	switch(*sex) {
 		case 'f':
-			if(BMI < 19)
-				printf("Underweight");
-            else if(BMI < 25)
-                printf("Acceptable");
-            else if(BMI < 30)
-                printf("Overweight");
-            else if(BMI < 40)
-                printf("Obese");
+			if(*BMI < 19)
+				printf("A BMI of %.2f is considered underweight.", *BMI);
+            else if(*BMI < 25)
+                printf("A BMI of %.2f is considered acceptable.", *BMI);
+            else if(*BMI < 30)
+                printf("A BMI of %.2f is considered overweight.", *BMI);
+            else if(*BMI < 40)
+                printf("A BMI of %.2f is considered obese.", *BMI);
             else
-                printf("Morbidly Obese");
+                printf("A BMI of %.2f is considered morbidly obese.", *BMI);
             break;
         case 'm':
-            if(BMI < 20)
-				printf("Underweight");
-            else if(BMI < 25)
-                printf("Acceptable");
-            else if(BMI < 30)
-                printf("Overweight");
-            else if(BMI < 40)
-                printf("Obese");
+            if(*BMI < 20)
+				printf("A BMI of %.2f is considered underweight.", *BMI);
+            else if(*BMI < 25)
+                printf("A BMI of %.2f is considered acceptable.", *BMI);
+            else if(*BMI < 30)
+                printf("A BMI of %.2f is considered overweight.", *BMI);
+            else if(*BMI < 40)
+                printf("A BMI of %.2f is considered obese.", *BMI);
             else
-                printf("Morbidly Obese");
+                printf("A BMI of %.2f is considered morbidly obese.", *BMI);
             break;
         default:
-            printf("Unknown");
+            printf("Unknown");    
 	}
-	return 0;
+}
+
+void main() {
+	float BMI, height, weight;
+	char sex;
+	int i = 1;
+
+	do{
+		printf("BMI calculator\n");
+		printf("Enter sex [f/m]\t  : ");
+		scanf(" %c",&sex);
+		sex = tolower(sex);
+		printf("Enter weight [kg] : ");
+		scanf(" %f",&weight);
+		printf("Enter height [cm] : ");
+		scanf(" %f",&height);
+		BMI = weight / ((height * 0.01) * (height * 0.01));
+		
+		calculation(&sex, &BMI);
+		printf("\n\n");		
+	} while(i == 1);
 }
