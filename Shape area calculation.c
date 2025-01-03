e0bd6fdc9dbe91317e0cebfb7b5b2cea7e0c652a
James28Dev <james28.dev@gmail.com>
2025-01-03 19:15:31 +0700

    create shape area calculation

---
+++ Shape area calculation.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+void main() {
+	char shapeId;
+	float total, num1, num2;
+	int i = 1;
+	
+	do{
+		printf("What shape do you want?\n");
+	printf("1. Triangle\t[T/t]\n");
+	printf("2. Regtangle\t[R/r]\n");
+	printf("3. Circle\t[C/c]\n");
+	printf("Enter shape : ");
+	scanf("%c",&shapeId);
+	shapeId = tolower(shapeId);
+	printf("--------------------------\n");
+
+	switch(shapeId) {
+		case 't':
+			printf("\tTriangle\n");
+			printf("Enter base : ");
+			scanf("%f",&num1);
+			printf("Enter high : ");
+			scanf("%f",&num2);
+
+			total = num1 * num2 * 0.5;
+			printf("Area = %.2f\n",total);
+			break;
+		case 'r':
+			printf("\tRegtangle\n");
+			printf("Enter wide : ");
+			scanf("%f",&num1);
+			printf("Enter long : ");
+			scanf("%f",&num2);
+
+			total = num1 * num2;
+			printf("Area = %.2f\n",total);
+			break;
+		case 'c':
+			printf("\tCircle\n");
+			printf("Enter radius : ");
+			scanf("%f",&num1);
+
+			total = num1 * num1 * 3.14;
+			printf("Area = %.2f\n",total);
+			break;
+		default:
+			printf("Unknow shape ID %c\n",shapeId);
+	}
+	printf("--------------------------\n");
+	} while(i == 1);
+}
