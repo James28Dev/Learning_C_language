8dcff8d28eb4ce6fb2fd3f7ade0d4819bee20998
James28Dev <james28.dev@gmail.com>
2025-01-03 19:34:17 +0700

    shape area calculation 1.1

--- Shape area calculation.c
+++ Shape area calculation.c
@@ -7,46 +7,52 @@ void main() {
 	
 	do{
 		printf("What shape do you want?\n");
-	printf("1. Triangle\t[T/t]\n");
-	printf("2. Regtangle\t[R/r]\n");
-	printf("3. Circle\t[C/c]\n");
-	printf("Enter shape : ");
-	scanf("%c",&shapeId);
-	shapeId = tolower(shapeId);
-	printf("--------------------------\n");
-
-	switch(shapeId) {
-		case 't':
-			printf("\tTriangle\n");
-			printf("Enter base : ");
-			scanf("%f",&num1);
-			printf("Enter high : ");
-			scanf("%f",&num2);
-
-			total = num1 * num2 * 0.5;
-			printf("Area = %.2f\n",total);
-			break;
-		case 'r':
-			printf("\tRegtangle\n");
-			printf("Enter wide : ");
-			scanf("%f",&num1);
-			printf("Enter long : ");
-			scanf("%f",&num2);
-
-			total = num1 * num2;
-			printf("Area = %.2f\n",total);
-			break;
-		case 'c':
-			printf("\tCircle\n");
-			printf("Enter radius : ");
-			scanf("%f",&num1);
-
-			total = num1 * num1 * 3.14;
-			printf("Area = %.2f\n",total);
-			break;
-		default:
-			printf("Unknow shape ID %c\n",shapeId);
-	}
-	printf("--------------------------\n");
+		printf("1. Triangle\t[T/t]\n");
+		printf("2. Regtangle\t[R/r]\n");
+		printf("3. Circle\t[C/c]\n");
+		printf("4. Exit Program\t[E/e]\n");
+		printf("Enter shape : ");
+		scanf(" %c",&shapeId);
+		shapeId = tolower(shapeId);
+		printf("--------------------------\n");
+		
+		switch(shapeId) {
+			case 't':
+				printf("\tTriangle\n");
+				printf("Enter base : ");
+				scanf("%f",&num1);
+				printf("Enter high : ");
+				scanf("%f",&num2);
+	
+				total = num1 * num2 * 0.5;
+				printf("Area = %.2f\n",total);
+				printf("--------------------------\n\n");
+				break;
+			case 'r':
+				printf("\tRegtangle\n");
+				printf("Enter wide : ");
+				scanf("%f",&num1);
+				printf("Enter long : ");
+				scanf("%f",&num2);
+	
+				total = num1 * num2;
+				printf("Area = %.2f\n",total);
+				printf("--------------------------\n\n");
+				break;
+			case 'c':
+				printf("\tCircle\n");
+				printf("Enter radius : ");
+				scanf("%f",&num1);
+	
+				total = num1 * num1 * 3.14;
+				printf("Area = %.2f\n",total);
+				printf("--------------------------\n\n");
+				break;
+			case 'e':
+				i = 2;
+				break;
+			default:
+				printf("Unknow shape ID %c\n",shapeId);
+		}
 	} while(i == 1);
 }
