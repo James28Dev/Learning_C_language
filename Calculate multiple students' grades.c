60368096a94d4e80aef11129ddfa9b61e867547d
James28Dev <james28.dev@gmail.com>
2025-01-16 01:25:49 +0700

    Update Calculate multiple students' grades.c

--- Calculate multiple students' grades.c
+++ Calculate multiple students' grades.c
@@ -11,15 +11,15 @@ char grade (int score) {
         return 'D';
     } else {
         return 'F';
-    }
-}
+    } //end score if
+} //end grade function
 
 inputScore (int i, int score[], int num) {
 	for (i = 0; i < num; i++) {
         printf("Input Score [%d] : ", i + 1);
-        scanf(" %d", &score[i]);
-    }
-}
+        scanf(" %d", &score[i]); //get score
+    } //end for loops
+} //end inputScore function
 
 int main () {
 	int score[20] ,i, numStudent;
@@ -28,18 +28,17 @@ int main () {
 	printf("Program calculate multiple students' grades\n");
 	printf("------------------------------------------\n");
 	printf("Number of students to be calculated = ");
-	scanf(" %d", &numStudent);
-    inputScore(i, score, numStudent);
+	scanf(" %d", &numStudent); //get numStudent
+    inputScore(i, score, numStudent); //call inputScore function
 
     printf("\n==========================================\n");
 	printf("Sudents\t\t Score\t\t Grade\n");
 	printf("==========================================\n");
 	
     for (i = 0; i < numStudent; i++) {
-        total_grade[i] = grade(score[i]);
+        total_grade[i] = grade(score[i]); //call grade function
         printf("%d\t\t %d\t\t %c\n", i + 1, score[i], total_grade[i]);
-        
-    }
+    } //end for loops
 
     return 0;
-}
+} //end main function
