342882eff811bdd9a6f6641d3fd9ebf1252cce3f
James28Dev <james28.dev@gmail.com>
2025-01-16 01:25:53 +0700

    Update Even Number Finder.c

--- Even Number Finder.c
+++ Even Number Finder.c
@@ -7,26 +7,26 @@ void Show(int start, int stop) {
 		for(i = start; i <= stop; i++) {
 			if(i % 2 == 0)
 				printf("%d ", i);
-		}
-	} 
+		} //end for loops
+	} //end start < stop if 
 	else {
 		printf("even >> ");
 		int i;
 		for(i = start; i >= stop; i--) {
 			if(i % 2 == 0)
 				printf("%d ", i);
-		}
-	}
-}
+		} //end for loops
+	} //end else
+} //end Show function
 
 int main() {
 	int start, stop;
 	
 	printf("Start Number : ");
-	scanf("%d",&start);
+	scanf("%d",&start); //get start
 	printf("Stop Number : ");
-	scanf("%d",&stop);
+	scanf("%d",&stop); //get stop
 	
-	Show(start, stop);
+	Show(start, stop); //call Show function
 	return 0;
-}
+} //end main function
