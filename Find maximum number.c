6266cf4046cb8eddc7ea6369ee3a6785176a6573
James28Dev <james28.dev@gmail.com>
2025-01-16 01:26:00 +0700

    Update Find maximum number.c

--- Find maximum number.c
+++ Find maximum number.c
@@ -4,42 +4,42 @@ struct dataNumber {
 	int num1;
 	int num2;
 	char choice;
-} data;
+} data; //end dataNumber structure
 
 Input(int *num1, int *num2) {
 	printf("  Find maximum number\n");
 	printf("------------------------\n");
 	printf("Number 1 : ");
-	scanf("%d", &data.num1);
+	scanf("%d", &data.num1); //get num1 in dataNumber structure
 	printf("Number 2 : ");
-	scanf("%d", &data.num2);
-}
+	scanf("%d", &data.num2); //get num2 in dataNumber structure
+} //end Input function
 
 Process(int *num1, int *num2) {
-	if(*num1 > *num2 || *num1 == *num2)
-		return *num1;
+	if(*num1 >= *num2)
+		return *num1; //end num1 >= num2 if
 	else if(*num1 < *num2)
-		return *num2;
-}
+		return *num2; //end else
+} //end Process function
 
 Loop(char *choice) {
 	printf("------------------------\n");
 	printf("Again? (Y/N) ");
 	char temp;
-	scanf(" %c", &temp);
+	scanf(" %c", &temp); //get temp
 	temp = toupper(temp);
 	*choice = temp;
 	printf("\n");
-}
+} //end Loop function
 
 void main() {
 	do {
-		Input(&data.num1, &data.num2);
-		printf("Maximum = %d", Process(&data.num1, &data.num2));
+		Input(&data.num1, &data.num2); //call Input
+		printf("Maximum = %d", Process(&data.num1, &data.num2)); //call Process
 		if(data.num1 == data.num2)
-			printf(" Equals\n");
+			printf(" Equals\n"); //end num1 in structure == num2 in structure if
 		else
-			printf("\n");
-		Loop(&data.choice);
-	} while(data.choice == 'Y');
-}
+			printf("\n"); //end else
+		Loop(&data.choice); //call Loop function
+	} while(data.choice == 'Y'); //end do-while loops
+} //end main function
